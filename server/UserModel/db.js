3ab3424fdba5ed6abbd96288e664b7017491eb85
DharmikNarola111 <hellonarola@gmail.com>
2023-05-03 21:08:20 -0400

    update console message

--- server/UserModel/db.js
+++ server/UserModel/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://dharmik:dharmik@cluster0.razemee.mongodb.net/EMsystem');
 mongoose.connection.on("connected", function(){
-    console.log("Application is connected to Databse");
+    console.log("Application is connected to Database");
 })
 
